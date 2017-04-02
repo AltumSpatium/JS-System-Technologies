bba3757917efeb1ed8391d52513935ddacfc21e7
Alex Martynov <sinner.in.me11@gmail.com>
2017-04-03 01:51:06 +0300

    Added second homework

---
+++ Homework 02/task1.js
@@ -0,0 +1,43 @@
+"use strict";
+
+function filterUniqueObj(objArray, filterArray) {
+	let checkedObjects = [];
+
+	return objArray.filter(function(item) {
+		let objStr = "";
+
+		for (let filterProperty of filterArray) {
+			objStr += filterProperty + item[filterProperty];
+		}
+
+		if (checkedObjects.indexOf(objStr) !== -1) {
+			return false;
+		} else {
+			checkedObjects.push(objStr);
+			return true;
+		}
+	});
+}
+
+function addStringRepresentation(objArray) {
+	for (let obj of objArray) {
+		obj.toString = () => {
+			let str = '{';
+			for (let prop in obj) {
+				if (typeof obj[prop] !== 'function') {
+					str += prop + ': ' + obj[prop] + ', ';
+				}
+			}
+			return str.slice(0, -2) + '}';
+		}
+	}
+}
+
+function task1() {
+	let objArray = [{a: 1, b: 1, c: 1}, {a: 1, b: 1}, {a: 2}, {a: 1, b: 2}];
+	addStringRepresentation(objArray);
+	alert(filterUniqueObj(objArray, ['a']));
+	alert(filterUniqueObj(objArray, ['b']));
+	alert(filterUniqueObj(objArray, ['a', 'b']));
+	alert(filterUniqueObj(objArray, ['f']));
+}
---
+++ Homework 02/task2.js
@@ -0,0 +1,35 @@
+"use strict";
+
+function getThirdMin(array) {
+	if (array.length < 3) {
+		return undefined;
+	}
+
+	let [firstMin, secondMin, thirdMin] = array.slice(0, 3).sort();
+
+	for (let elem of array.slice(3)) {
+		if (elem < firstMin) {
+			[thirdMin, secondMin, firstMin] = [secondMin, firstMin, elem];
+		} else if (elem < secondMin) {
+			[thirdMin, secondMin] = [secondMin, elem];
+		} else if (elem < thirdMin) {
+			thirdMin = elem;
+		}
+	}
+
+	return thirdMin;
+}
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function task2() {
+	let array = [];
+	for (let i = 0; i < 100; i++) {
+		array.push(getRandomInt(500, 1000));
+	}
+
+	let thirdMin = getThirdMin(array);
+	alert(thirdMin);
+}
