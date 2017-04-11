e398776bf6d1bda8e4e08b87de495866368d3842
Alex Martynov <sinner.in.me11@gmail.com>
2017-04-12 01:50:28 +0300

    Added task in ES6 style

--- Homework 04/task3.js
+++ Homework 04/task3.js
@@ -1,72 +1,76 @@
 'use strict';
 
-function Kingdom(description) {
-	let kingdomName = description.kingdomName;
-	this._isAutotroph = description.isAutotroph;
-	this._isUnicellular = description.isUnicellular;
+class Kingdom {
+	constructor(description) {
+		this._kingdomName = description.kingdomName;
+		this._isAutotroph = description.isAutotroph;
+		this._isUnicellular = description.isUnicellular;
+	}
 
-	this.getKingdom = function() {
-		return kingdomName;
-	};
+	getKingdom() {
+		return this._kingdomName;
+	}
 
-	this.getInfo = function() {
+	getInfo() {
 		let info = 'Царство: ' + this.getKingdom() + '\n';
 		info += 'Категория: ' + (this._isUnicellular ? 'одноклеточные' : 'многоклеточные') + '\n';
 		info += 'Способ питания: ' + (this._isAutotroph ? 'автотрофный' : 'гетеротрофный') + '\n';
 		return info;
-	};
+	}
 }
 
-function Class(description) {
-	Kingdom.apply(this, arguments);
+class Class extends Kingdom {
+	constructor(description) {
+		super(description);
 
-	let className = description.className;
+		this._className = description.className;
+	}
 
-	this.getClass = function() {
-		return className;
-	};
+	getClass() {
+		return this._className;
+	}
 
-	let parentGetInfo = this.getInfo;
-	this.getInfo = function() {
+	getInfo() {
 		let info = 'Класс: ' + this.getClass() + '\n';
-		info += parentGetInfo.call(this);
+		info += super.getInfo();
 		return info;
-	};
+	}
 }
 
-function Species(description) {
-	Class.apply(this, arguments);
+class Species extends Class {
+	constructor(description) {
+		super(description);
 
-	let speciesName = description.speciesName;
-	let lifespan = description.lifespan > 0 ? description.lifespan : 1;
-	this._habitat = description.habitat;
-	this._isCarnivorous = description.isCarnivorous;
+		this._speciesName = description.speciesName;
+		this._lifespan = description.lifespan > 0 ? description.lifespan : 1;
+		this._habitat = description.habitat;
+		this._isCarnivorous = description.isCarnivorous;
+	}
 
-	this.getSpecies = function() {
-		return speciesName;
-	};
+	getSpecies() {
+		return this._speciesName;
+	}
 
-	this.setLifespan = function(newLifespan) {
-		lifespan = newLifespan > 0 ? newLifespan : 1;
-	};
+	setlifespan(newLifespan) {
+		this._lifespan = newLifespan > 0 ? newLifespan : 1;
+	}
 
-	this.getLifespan = function() {
-		return lifespan;
-	};
+	getLifespan() {
+		return this._lifespan;
+	}
 
-	let parentGetInfo = this.getInfo;
-	this.getInfo = function() {
+	getInfo() {
 		let info = 'Вид: ' + this.getSpecies() + '\n';
-		info += parentGetInfo.call(this);
+		info += super.getInfo();
 		info += 'Продолжительность жизни: ' + this.getLifespan() + 
 			(this.getLifespan() % 10 < 5 && this.getLifespan() % 10 > 0 ? 'г.' : 'л.') + '\n';
 		info += 'Ареал: ' + this._habitat + '\n';
 		info += 'Отряд: ' + (this._isCarnivorous ? 'Плотоядные' : 'Травоядные') + '\n';
 		return info;
-	};
+	}
 }
 
-function task1() {
+function task3() {
 	let butterfly = new Species({kingdomName: 'Животные', isAutotroph: false,
 		isUnicellular: false, className: 'Насекомые', speciesName: 'Бабочка-капустница',
 		lifespan: 1, habitat: 'Восточная Европа, Северная Африка', isCarnivorous: false});
